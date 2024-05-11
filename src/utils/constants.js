01aff9db6b4191592862c94c8f669f9972cd115f
Deepak Rajput <deepak.onlinework13@gmail.com>
2024-05-11 11:49:19 +0530

    l-15 creating tmdb account, API KEY and access_token, create constants for API options and call api in Browse.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
 // export const PHOTO_URL = "https://avatars.githubusercontent.com/u/101570965?v=4"
-export const PHOTO_URL = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
+export const PHOTO_URL = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Yzk3N2Q1NjA5ZTY5MzlmZmFkMGNlYmEzMWZmZjlmYSIsInN1YiI6IjY2M2YwNzFhMjBmMWU0YjNlMGEzZTVmOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6V8fqmknUmL1vUbI9i_jjiAw8RvLw0hNev2NO0-rtQw'
+    }
+}
