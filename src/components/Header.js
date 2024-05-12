7f7ddec0034b0f4f82392968ae50b7d87b40f97a
Deepak Rajput <deepak.onlinework13@gmail.com>
2024-05-12 06:04:43 +0530

    l-16 building multi language pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,8 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser } from '../utils/userSlice';
 import { toggleGptSearchView } from '../utils/gptSlice';
+import { SUPPORTED_LANGUAGES } from '../utils/constants';
+import { changeLanguage } from '../utils/configSlice';
 
 const Header = () => {
 
@@ -14,7 +16,9 @@ const Header = () => {
   const dispatch = useDispatch();
 
   const user = useSelector((store) => store.user);
-  console.log("user_Header", user)
+  console.log("user_Header", user);
+
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch)
 
   const handleSignOut = () => {
     signOut(auth).then(() => {
@@ -29,6 +33,11 @@ const Header = () => {
   const handleGptSearchClick = () => {
     // toggle GptSearch
     dispatch(toggleGptSearchView())
+  }
+
+  const handleLanguageChange = (e) => {
+    // console.log('lang', e.target.value);
+    dispatch(changeLanguage(e.target.value))
   }
 
   useEffect(() => {
@@ -61,7 +70,14 @@ const Header = () => {
       />
       {
         user && <div className='flex p-2'>
-          <button className='py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg' onClick={handleGptSearchClick}>GPT Search</button>
+          {showGptSearch && <select className='p-2 px-3 m-2 bg-gray-900 text-white rounded-lg' onChange={handleLanguageChange}>
+            {
+              SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>
+              ))
+            }
+          </select>}
+          <button className='py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg' onClick={handleGptSearchClick}>{showGptSearch ? "Home" : "GPT Search"}</button>
           <img
             className='w-12 h-12 '
             alt='usericon'
